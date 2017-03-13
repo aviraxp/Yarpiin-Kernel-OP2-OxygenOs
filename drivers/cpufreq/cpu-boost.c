81c2c88005589df3948250e415f4db3b05a3fe58
Slawek <yarpiin@gmail.com>
2017-03-13 18:26:05 +0000

    cpufreq: cpu-boost: Disable input boost by default
    credits Jollaman

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -57,7 +57,8 @@ module_param(sysctl_thermal_aware_scheduling, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static bool input_boost_enabled;
+static unsigned int input_boost_enabled = 0;
+module_param(input_boost_enabled, uint, 0644);
 
 static unsigned int input_boost_ms = 40;
 module_param(input_boost_ms, uint, 0644);
