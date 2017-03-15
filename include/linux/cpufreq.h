da3c4df62f90dfc76438852aef7243c63c9d1b6b
Slawek <yarpiin@gmail.com>
2017-03-15 18:23:24 +0000

    Revert "add Lionheart gov"
    This reverts commit 6cce8897b540166674830e41b4ca1e9010f0875d.

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -477,9 +477,6 @@ extern struct cpufreq_governor cpufreq_gov_darkness;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_despair)
 extern struct cpufreq_governor cpufreq_gov_despair;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_DESPAIR)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
-extern struct cpufreq_governor cpufreq_gov_lionheart;
-#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONFISH)
 extern struct cpufreq_governor cpufreq_gov_lionfish;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_lionfish)
